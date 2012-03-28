ff748a771bf315063eb3b61005845af116e12a48
Patricio Cano <suprnova32@gmail.com>
2012-03-28 08:34:44 +0200

    Functionality added

--- ConnectivityTest.py
+++ ConnectivityTest.py
@@ -5,5 +5,10 @@ __author__ = 'Patricio Cano'
 import argparse, ConnectionTestClass as vpn
 #Config File necessary.
 
-testAgent = vpn.ConnectionTestClass()
+parser = argparse.ArgumentParser(description='ASA VPN Connectivity Test:')
+parser.add_argument('-v', dest='boolean_switch', action='store_true', default=False, help='Set verbose to true.')
+results = parser.parse_args()
+
+
+testAgent = vpn.ConnectionTestClass(verbose=results.boolean_switch)
 testAgent.beginTest()
